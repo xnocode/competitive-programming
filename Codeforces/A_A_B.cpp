7dfeb3e06c7655edda8f5f0fc57d46393c313a6b
Ridoy <xridoynocode@gmail.com>
2026-01-06 20:02:54 +0600

    Add A_A_B.cpp solution to Codeforces problem
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ Codeforces/A_A_B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c, a, b;
+    cin >> c;
+    char p;
+    while (c--)
+    {
+        cin >> a >> p >> b;
+        cout << a + b << endl;
+    }
+}
