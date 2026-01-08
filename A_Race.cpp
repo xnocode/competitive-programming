6c108086a290d53eceff63d46431c3017fc5182e
Ridoy <xridoynocode@gmail.com>
2026-01-08 19:39:54 +0600

    Race
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ A_Race.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int a, x, y;
+        cin >> a >> x >> y;
+        if (a > x && a > y)
+            cout << "YES" << endl;
+        else if (a < x && a < y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
