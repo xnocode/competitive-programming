f28f014c6e4e307c8b2a8f6a8576746c43097198
Ridoy <xridoynocode@gmail.com>
2026-01-10 21:36:23 +0600

    ASCII
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ Codeforces/A_ASCII_Art_Contest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 3);
+    if (arr[2] - arr[0] >= 10)
+    {
+        cout << "check again";
+    }
+    else
+    {
+        cout << "final " << arr[1];
+    }
+}
